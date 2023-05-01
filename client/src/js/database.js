64b911c93e3289dc452c036b980c8cb9ec1e1a46
tbgolfer86 <tbj86@outlook.com>
2023-04-30 20:42:49 -0400

    corrected data privileges for putDb method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,7 @@ export const putDb = async (content) => {
   // Create a connection to the database and version we want to use.
   const jateDb = await openDB('jate', 1);
   // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'readonly');
+  const tx = jateDb.transaction('jate', 'readwrite');
   // Open up the desired object store.
   const store = tx.objectStore('jate');
   // Use the .put() method to insert data into the database.
