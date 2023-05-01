c8298e31c34a01b68b96d523b57eab29dc4181bd
tbgolfer86 <tbj86@outlook.com>
2023-04-30 23:19:14 -0400

    added StaleWhileRevalidate to require and fixed get method

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -30,7 +30,7 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 registerRoute(
   // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new CacheFirst({
+  new StaleWhileRevalidate({
     // Name of the cache storage.
     cacheName: 'asset-cache',
     plugins: [
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -39,8 +39,8 @@ export const getDb = async () => {
   const tx = jateDb.transaction('jate', 'readonly');
   // Open up the desired object store.
   const store = tx.objectStore('jate');
-  // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  // Use the .get() method to get all data in the database.
+  const request = store.get('jate');
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
